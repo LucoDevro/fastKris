6734810f008d8a4449a83be271e2e8e069c4d00b
Lucas De Vrieze <99831106+LucoDevro@users.noreply.github.com>
2022-10-25 20:11:06 +0200

    Calculations module ready

--- main.py
+++ main.py
@@ -18,16 +18,20 @@ def run(protocol: protocol_api.ProtocolContext):
     print('Compound library loaded...')
     [tips, instruments, plates, screens]=setup(protocol)
     print('Screens parsed...')
-    for i in screens:
-        print(i)
+    screen_vols=[s.getAllVol() for s in screens]
+    print(screen_vols)
         
-    reservoir = protocol.load_labware('nest_12_reservoir_15ml', 8)
-    instruments[0].transfer(100, reservoir['A1'], plates[0].wells())
+    reservoir_salt = protocol.load_labware('nest_12_reservoir_15ml', 7)
+    reservoir_buffer = protocol.load_labware('nest_12_reservoir_15ml', 8)
+    reservoir_prec = protocol.load_labware('nest_12_reservoir_15ml', 9)
+    reservoir_MQ = protocol.load_labware('nest_12_reservoir_15ml', 6)
+    
+#    instruments[0].transfer(100, reservoir['A1'], plates[0].wells())
 
-    for i in range(8):
-        row = plates[0].rows()[i]
-        instruments[0].transfer(100, reservoir['A2'], row[0], mix_after=(3, 50))
-        instruments[0].transfer(100, row[:11], row[1:], mix_after=(3, 50))
+#    for i in range(8):
+#        row = plates[0].rows()[i]
+#        instruments[0].transfer(100, reservoir['A2'], row[0], mix_after=(3, 50))
+#        instruments[0].transfer(100, row[:11], row[1:], mix_after=(3, 50))
         
 def setup(protocol):
     with open('inputdata2.txt') as f:
@@ -58,33 +62,43 @@ def setup(protocol):
     
     # loading plates
     plates=[protocol.load_labware(platesInput[i], platesLocation[i]) for i in range(len(platesInput))]
+    print(protocol.loaded_labwares)
         
     # parsing screens
     screens_raw=lines[3].split('\n')[:-1]
     screens=[]
-    all_screen_types=screens_raw[0::6]
-    all_screen_compounds=screens_raw[1::6]
-    all_screen_indices=screens_raw[2::6]
-    all_screen_ranges=screens_raw[3::6]
-    all_screen_plates=screens_raw[4::6]
-    all_screen_workVol=screens_raw[5::6]
+    all_screen_types=screens_raw[0::5]
+    all_screen_compounds=screens_raw[1::5]
+    all_screen_ranges=screens_raw[2::5]
+    all_screen_plates=screens_raw[3::5]
+    all_screen_workVol=screens_raw[4::5]
     numberOfScreens=len(all_screen_types)
     for s in range(numberOfScreens):
+        
+        # screen type (1D, 2D or 3D)
         screen_type=int(all_screen_types[s])
+        
+        # parse plate number and link the plate object to it
         screen_plate=int(all_screen_plates[s])
+        screen_plate=protocol.loaded_labwares[screen_plate]
+        
+        # well working volume
         screen_workVol=int(all_screen_workVol[s])
+        
+        # compound used in this particular screen
         screen_compounds=[compoundLibrary[i] for i in all_screen_compounds[s].split(',')]
-        screen_indices=[int(i) for i in all_screen_indices[s].split(',')]
+        
+        # parsing compound concentration ranges and linking them to the associated compound
         screen_range_raw=all_screen_ranges[s].split(',')
         screen_range={}
-        for c in range(len(screen_indices)):
-            screen_range[screen_compounds[screen_indices[c]]]=tuple([float(i) for i in screen_range_raw[c].split('-')])
-        
-        # creating Screen objects
+        for c in range(len(screen_range_raw)):
+            screen_range[screen_compounds[c]]=tuple([float(i) for i in screen_range_raw[c].split('-')])
+                
+        # creating Screen objects from the parsed properties, depending on the screen type
         if screen_type==1:
-            screen=oneD(screen_indices, screen_range, screen_compounds, screen_plate, screen_workVol)
+            screen=oneD(screen_range, screen_compounds, screen_plate, screen_workVol)
         elif screen_type==2:
-            screen=twoD(screen_indices, screen_range, screen_compounds, screen_plate, screen_workVol)
+            screen=twoD(screen_range, screen_compounds, screen_plate, screen_workVol)
         elif screen_type==3:
             screen=threeD(screen_range, screen_compounds, screen_plate, screen_workVol)
         else:
@@ -105,11 +119,11 @@ def loadLibrary():
     compLibrary={}
     for i in range(len(labels)):
         if types[i].lower()=="salt":
-            compLibrary[labels[i]] = Salt(stockConc[i], labels[i])
+            compLibrary[labels[i]] = Salt(float(stockConc[i]), labels[i])
         elif types[i].lower()=="precipitate":
-            compLibrary[labels[i]] = Precipitate(stockConc[i], labels[i])
+            compLibrary[labels[i]] = Precipitant(float(stockConc[i]), labels[i])
         elif types[i].lower()=="buffer":
-            compLibrary[labels[i]] = Buffer(stockConc[i], labels[i])
+            compLibrary[labels[i]] = Buffer(float(stockConc[i]), labels[i])
         else:
             raise Exception("Compound type not understood.")
             
@@ -133,73 +147,86 @@ class Screen:
     def calcConcentration(self,compound, outConc):
         outVol = []
         for conc in outConc:
-            outVol.append(compound.dilute(conc, self.workVol))
+            outVol.append(round(compound.dilute(conc, self.workVol),3)) # rounding up to 0.001 uL
         return outVol
 
 class oneD(Screen):
-    def __init__(self, index, *args, **kwargs):
-        self.index = index #a list
-        super().__init__(*args, **kwargs)
         
     def __str__(self):
-        return '1D screen, ' + str(self.range) + str(self.compounds) + ' on plate ' + str(self.plate) + ' (' + str(self.workVol) + ')'
+        return '1D screen, ' + str(self.range) + str(self.compounds) + ' on plate ' + str(self.plate) + ' (' + str(self.workVol) + ' uL)'
 
     def getOutConc(self, compound):
         #get np array
         return np.linspace(start=self.range[compound][0], stop=self.range[compound][1], num=len(self.plate.wells())).tolist()
-
+    
     def getAllVol(self):
         dict = {}
-        outConc = self.getOutConc(self.compounds[self.index[0]])
-        out = self.calcConcentration(self.compounds[self.index[0]],outConc)
-        dict[self.compounds[self.index[0]]] = out
-        #add fixed volumes other compounds when constructing input file
+        totVol = np.zeros(len(self.plate.rows()[0])*len(self.plate.columns()[0]))
+        for compound in self.compounds:
+            outConc = self.getOutConc(compound)
+            out = self.calcConcentration(compound,outConc)
+            dict[compound] = out
+            totVol += np.array(out)
+        dict['MQ'] = np.round(self.workVol - totVol, 3).flatten().tolist()
         return dict
-
+            
 class twoD(Screen):
-
-    def __init__(self, index, *args, **kwargs):
-        self.index = index #a list
-        super().__init__(*args, **kwargs)
         
     def __str__(self):
-        return '2D screen, ' + str(self.range) + str(self.compounds) + ' on plate ' + str(self.plate) + ' (' + str(self.workVol) + ')'
+        return '2D screen, ' + str(self.range) + str(self.compounds) + ' on plate ' + str(self.plate) + ' (' + str(self.workVol) + ' uL)'
 
     def getOutConc(self, compound):
         if isinstance(compound,Salt):
             return np.linspace(start=self.range[compound][0], stop=self.range[compound][1], num=len(self.plate.rows()[0])).tolist()
-        if isinstance(compound, Precipitate):
+        if isinstance(compound, Precipitant):
             return np.linspace(start=self.range[compound][0], stop=self.range[compound][1], num=len(self.plate.columns()[0])).tolist()
-
+        if isinstance(compound, Buffer):
+            return np.linspace(start=self.range[compound][0], stop=self.range[compound][1], num=len(self.plate.wells())).tolist()
+        
     def getAllVol(self):
         dict = {}
-        for ind in self.index:
-            outConc = self.getOutConc(self.compounds[ind])
-            out = self.calcConcentration(self.compounds[ind],outConc)
-            dict[self.compounds[ind]] = out
-        #add fixed volumes other compounds when constructing input file
+        totVol = np.zeros(len(self.plate.rows()[0])*len(self.plate.columns()[0]))
+        for compound in self.compounds:
+            outConc = self.getOutConc(compound)
+            out = self.calcConcentration(compound,outConc)
+            if isinstance(compound, Salt):
+                out = np.tile(np.array(out), (len(self.plate.columns()[0]), 1)).flatten().tolist()
+            elif isinstance(compound, Precipitant):
+                out = np.tile(np.array(out), (len(self.plate.rows()[0]), 1)).transpose().flatten().tolist()
+            dict[compound] = out
+            totVol += np.array(out)
+        dict['MQ'] = np.round(self.workVol - totVol, 3).flatten().tolist()
         return dict
 
+# Identical to 2D class at the moment
 class threeD(Screen):
     
     def __str__(self):
-        return '3D screen, ' + str(self.range) + str(self.compounds) + ' on plate ' + str(self.plate) + ' (' + str(self.workVol) + ')'
+        return '3D screen, ' + str(self.range) + str(self.compounds) + ' on plate ' + str(self.plate) + ' (' + str(self.workVol) + ' uL)'
 
     def getOutConc(self, compound):
         if isinstance(compound,Salt):
             return np.linspace(start=self.range[compound][0], stop=self.range[compound][1], num=len(self.plate.rows()[0])).tolist()
-        if isinstance(compound, Precipitate):
+        if isinstance(compound, Precipitant):
             return np.linspace(start=self.range[compound][0], stop=self.range[compound][1], num=len(self.plate.columns()[0])).tolist()
-
+        if isinstance(compound, Buffer):
+            return np.linspace(start=self.range[compound][0], stop=self.range[compound][1], num=len(self.plate.wells())).tolist()
+        
     def getAllVol(self):
         dict = {}
+        totVol = np.zeros(len(self.plate.rows()[0])*len(self.plate.columns()[0]))
         for compound in self.compounds:
-            if isinstance(compound,Salt) or isinstance(compound, Precipitate):
-                outConc = self.getOutConc(compound)
-                out = self.calcConcentration(compound,outConc)
-                dict[compound] = out
+            outConc = self.getOutConc(compound)
+            out = self.calcConcentration(compound,outConc)
+            if isinstance(compound, Salt):
+                out = np.tile(np.array(out), (len(self.plate.columns()[0]), 1)).flatten().tolist()
+            elif isinstance(compound, Precipitant):
+                out = np.tile(np.array(out), (len(self.plate.rows()[0]), 1)).transpose().flatten().tolist()
+            dict[compound] = out
+            totVol += np.array(out)
+        dict['MQ'] = np.round(self.workVol - totVol, 3).flatten().tolist()
         return dict
-
+        
 # Compound class
 
 class Compound:
@@ -210,30 +237,24 @@ class Compound:
     
 class Salt(Compound):
     
-    def __init__(self, stock, label):
-        super().__init__(stock, label)
-    
     def __str__(self):
         return self.label + ', Salt'
         
     def dilute(self, outputConc, wellVol):
         return outputConc*wellVol/self.stock
     
-class Precipitate(Compound):
-    
-    def __init__(self, stock, label):
-        super().__init__(stock, label)
+class Precipitant(Compound):
 
     def __str__(self):
-        return self.label + ', Precipitate'
+        return self.label + ', Precipitant'
         
     def dilute(self, outputPerc, wellVol):
         return outputPerc*wellVol/self.stock
     
 class Buffer(Compound):
     
-    def __init__(self, stock, label):
-        super().__init__(stock, label)
-    
     def __str__(self):
         return self.label + ', Buffer'
+    
+    def dilute(self, outputConc, wellVol):
+        return wellVol/10
