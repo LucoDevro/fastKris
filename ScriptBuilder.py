9fe8a8bc77f385683e2057ea3b98047426323966
Sophie Colette <69583383+SC-Git1@users.noreply.github.com>
2022-11-27 22:06:52 +0100

    customize metadata values

--- ScriptBuilder.py
+++ ScriptBuilder.py
@@ -1,5 +1,7 @@
+import json
 import sys
 import csv
+
 
 # Python function
 def runScriptBuilder(filename, protocolFilePath):
@@ -17,7 +19,7 @@ def runScriptBuilder(filename, protocolFilePath):
         screens_raw = str(screens_raw[:-1])
     else:
         screens_raw = str(screens_raw)
-    
+
     # Reading compound library
     with open('compLibrary.txt', 'r') as l:
         reader = csv.reader(l, delimiter="\t")
@@ -41,7 +43,52 @@ def runScriptBuilder(filename, protocolFilePath):
         main.write('stockConc = ' + stockConc + '\n\n')
         main.write(template.read())
         template.close()
-        
+
+def BuildWithMetadata(filename, protocolFilePath,protocolname,description,author):
+    # Reading parameter file
+    with open(filename, 'r') as f:
+        lines = f.read()
+        lines = lines.split("\n\n")
+
+    tips_raw = str(lines[0].split("\n"))
+    tuberack_raw = str(lines[1].split("\n"))
+    instr_raw = str(lines[2].split("\n"))
+    plates_raw = str(lines[3].split("\n"))
+    screens_raw = lines[4].split('\n')
+    if screens_raw[-1] == '':
+        screens_raw = str(screens_raw[:-1])
+    else:
+        screens_raw = str(screens_raw)
+
+    # Reading compound library
+    with open('compLibrary.txt', 'r') as l:
+        reader = csv.reader(l, delimiter="\t")
+        lines = [entry for line in reader for entry in line]
+
+    labels = str(lines[0::3])
+    types = str(lines[1::3])
+    stockConc = str(lines[2::3])
+
+    # Writing the script from the template
+    template = open("template.py", "r")
+
+    with open(protocolFilePath, 'w') as main:
+        main.write(template.readline())
+        main.write("metadata = ")
+        main.write(json.dumps({"apiLevel":"2.9",'protocolName':protocolname,'description':description,'author':author}) + "\n")
+        main.write('tips_raw = ' + tips_raw + "\n")
+        main.write('tuberack_raw = ' + tuberack_raw + "\n")
+        main.write('instr_raw = ' + instr_raw + "\n")
+        main.write('plates_raw = ' + plates_raw + "\n")
+        main.write('screens_raw = ' + screens_raw + "\n\n")
+        main.write('labels = ' + labels + '\n')
+        main.write('types = ' + types + '\n')
+        main.write('stockConc = ' + stockConc + '\n\n')
+        for _ in range(9):
+            next(template)
+        main.write(template.read())
+        template.close()
+
 # bash command-line support
 if __name__ == "__main__":
     # calling without arguments triggers default file paths
@@ -53,5 +100,5 @@ if __name__ == "__main__":
         protocolpath = sys.argv[2]
     else:
         raise Exception("Wrong number of arguments! Expected a parameter file path and a protocol file path.")
-        
+
     runScriptBuilder(filename, protocolpath)
